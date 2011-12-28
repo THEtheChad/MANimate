c62ec5673a67b3df56101575a1b2e4da39ac083e
THEtheChad <chad.d.elliott@gmail.com>
2011-12-28 00:48:58 -0600

    comments out the wazoo

--- MANimate.js
+++ MANimate.js
@@ -4,7 +4,20 @@
  * Released under the MIT and GPL licenses.
  */
 
+// animate accepts a callback function that runs
+// inside an AnimationFrame loop. Each loop
+// walks over all animation callbacks given using
+// this function.
+//  
+// @input function
+//
+// Example:
+//      animate(function(){ return loopsUntilFalsy });
+//
 var animate = (function(w){
+
+    // vendor specific animation frame identification
+    // with setimeout fallback - Paul Irish
     var frame = w.requestAnimationFrame       || 
                 w.webkitRequestAnimationFrame || 
                 w.mozRequestAnimationFrame    || 
@@ -14,17 +27,26 @@ var animate = (function(w){
                     window.setTimeout(callback, 1000 / 60);
                 },
         queue = [],
-        loop = function(){
+        loop = function(timestamp){
             var i = queue.length;
 
+            // While there are callbacks to be performed
             while(i)
-                if(queue[--i]() == 0)
+                // Execute callback and remove the callback
+                // if there is a falsy response
+                if(queue[--i](timestamp) == 0)
                     queue.splice(i, 1);
             
+            // Continue the loop as long as there are callbacks
+            // in the queue
             queue.length && frame(loop);
         };
 
+    // Returns our public API
     return function(func){
+        // Add the callback to our queue
+        // If the queue was empty before,
+        // initiate the loop
         queue.push(func) == 1 && frame(loop);
     };
 })(window);
